76a9eff9b59bed4c27723afa4584f4deef28ca25
Bourne-ID <Bourne-ID@users.noreply.github.com>
2019-02-15 08:57:15 +0000

    Fix public key not loading in first instance

--- cloud/linode/setup.sh
+++ cloud/linode/setup.sh
@@ -15,20 +15,19 @@ fi
 # Read API file content to var
 export linodeapikey=`cat ${terra_linodeapikeyfile}`
 
-# Read the first line of the public key (ssh-keygen likes new lines...)
-export linodepublickey=`head ${terra_publickeylocation} -n 1`
-
 # Check to see if public and private key exists
 if [[ ! -f ./${terra_privatekeylocation} ]]; then
     echo "Private key ${terra_privatekeylocation} does not exist. Generating new private and public key. Please use this key to connect to the server over SSH"
     echo "The certificate does not have a passphrase - do not check into version control and treat it like a password!"
     ssh-keygen -a 100 -t ed25519 -N '' -f ./${terra_privatekeylocation}
     sleep 3
-
 else
     echo "Using private key ${terra_privatekeylocation}."
     sleep 3
 fi
+
+# Read the first line of the public key (ssh-keygen likes new lines...)
+export linodepublickey=`head ${terra_publickeylocation} -n 1`
 
 # Perform variable replacement on terraform template
 envsubst < linode.tf.template > linode.tf
